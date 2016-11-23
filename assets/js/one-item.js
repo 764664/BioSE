a166c45bc36759481e3ef46b06c404e99733a984
Jie Lu <764664@gmail.com>
2016-11-23 23:04:16 +0900

    Separate Modal

--- assets/js/one-item.js
+++ assets/js/one-item.js
@@ -63,6 +63,51 @@ export default class OneItem extends React.Component {
         }
     }
 
+    renderModal() {
+        return(
+            <Modal show={this.state.showModal} onHide={this.close.bind(this)} bsSize="large">
+              <Modal.Header closeButton>
+                <Modal.Title><a href={`/jump/`}>{this.props.paper.title}</a></Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+            <div className="div-authors">
+            {
+                this.props.paper.authors.map( (author) => {
+                return(
+                    <OverlayTrigger trigger={['focus']} placement="bottom" overlay={this.popover(author)}>
+                        <button type="button" className="btn btn-default btn-xs l10">
+                          {author}
+                        </button>
+                    </OverlayTrigger>
+                    )
+                })
+            }
+            </div>
+            <div className="meta-info">
+                <div className="meta-info-first">
+                <h6>{this.props.paper.journal}</h6>
+                <h6 className="pub-date">{this.props.paper.date}</h6>
+                </div>
+                    {
+                        this.renderSubscriptions()
+                    }
+            </div>
+            <div className="detail">
+                <a href={"/jump/"}>
+                    {this.props.paper.title}
+                </a>
+                <div className="abstract"
+                     dangerouslySetInnerHTML={{__html: this.props.paper.abstract.split(this.props.query).join("<span class=\"highlight\">" + this.props.query + "</span>")}}
+                />
+            </div>
+            </Modal.Body>
+              <Modal.Footer>
+                <Button onClick={this.close.bind(this)}>Close</Button>
+              </Modal.Footer>
+            </Modal>
+        )
+    }
+
     render() {
         return(
             <li className="list-group-item one-item">
@@ -93,46 +138,7 @@ export default class OneItem extends React.Component {
                             this.renderSubscriptions()
                         }
                 </div>
-                <Modal show={this.state.showModal} onHide={this.close.bind(this)} bsSize="large">
-                  <Modal.Header closeButton>
-                    <Modal.Title>{this.props.paper.title}</Modal.Title>
-                  </Modal.Header>
-                  <Modal.Body>
-                <div className="div-authors">
-                {
-                    this.props.paper.authors.map( (author) => {
-                    return(
-                        <OverlayTrigger trigger={['focus']} placement="bottom" overlay={this.popover(author)}>
-                            <button type="button" className="btn btn-default btn-xs l10">
-                              {author}
-                            </button>
-                        </OverlayTrigger>
-                        )
-                    })
-                }
-                </div>
-                <div className="meta-info">
-                    <div className="meta-info-first">
-                    <h6>{this.props.paper.journal}</h6>
-                    <h6 className="pub-date">{this.props.paper.date}</h6>
-                    </div>
-                        {
-                            this.renderSubscriptions()
-                        }
-                </div>
-                <div className="detail">
-                    <a href={"/jump/"}>
-                        {this.props.paper.title}
-                    </a>
-                    <div className="abstract"
-                         dangerouslySetInnerHTML={{__html: this.props.paper.abstract.split(this.props.query).join("<span class=\"highlight\">" + this.props.query + "</span>")}}
-                    />
-                </div>
-                </Modal.Body>
-                  <Modal.Footer>
-                    <Button onClick={this.close.bind(this)}>Close</Button>
-                  </Modal.Footer>
-                </Modal>
+                {this.renderModal()}
             </li>
         )
     }
