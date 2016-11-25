f2c8b28ab4f405524364a3461f8ad215f01d7d4b
Jie Lu <764664@gmail.com>
2016-11-26 02:58:35 +0900

    Can click on term to jump to webpage

--- assets/js/term_popover.js
+++ assets/js/term_popover.js
@@ -21,12 +21,19 @@ class TermPopover extends React.Component {
 }
 
 export default class Term extends React.Component {
-
+    renderURL() {
+        if (this.props.term.source == "MeSH") {
+            return `https://meshb.nlm.nih.gov/#/record/ui?ui=${this.props.term.oid}`;
+        }
+        else if (this.props.term.source == "GO") {
+            return `http://amigo.geneontology.org/amigo/term/${this.props.term.oid}`;
+        }
+    }
     render() {
         let term = this.props.term;
         var p =
         (
-            <Popover id="popover-positioned-top" title={term.name}>
+            <Popover id="popover-positioned-top" title={<a href={this.renderURL()} target="_blank">{term.name}</a>}>
                 <div className="definition">
                     {term.definition}
                 </div>
